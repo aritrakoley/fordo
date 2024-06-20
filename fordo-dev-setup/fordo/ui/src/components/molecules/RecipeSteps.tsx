7de9db2a7b656d6be10807147a2de34197c43c81
Aritra Koley <koley.aritra824@gmail.com>
2024-06-21 01:31:02 +0530

    Add steps component. Ingredient (WIP)

--- fordo-dev-setup/fordo/ui/src/components/molecules/RecipeSteps.tsx
+++ fordo-dev-setup/fordo/ui/src/components/molecules/RecipeSteps.tsx
@@ -8,11 +8,13 @@ const RecipeSteps = (props: RecipeStepsPropType) => {
     <div className="flex flex-col overflow-y-auto w-full flex-1 rounded-2xl mt-4">
       <ul className="w-full h-full">
         {steps?.length
-          ? steps.map((step, idx) => (
-              <li key={step.id} className="w-100">
-                <RecipeStep idx={idx + 1} step={step} />
-              </li>
-            ))
+          ? steps
+              .sort((a, b) => (a.sort_order ?? 0) - (b.sort_order ?? 0))
+              .map((step, idx) => (
+                <li key={step.id} className="w-100">
+                  <RecipeStep idx={idx + 1} step={step} />
+                </li>
+              ))
           : null}
       </ul>
     </div>
